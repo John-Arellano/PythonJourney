b3f1c679b76ec0757ed413051178fce0aa15444d
John Arellano Maroto <j.stevea@gmail.com>
2024-10-18 15:09:45 -0600

    Viernes-18-Oct

---
+++ PythonSyntax.py
@@ -0,0 +1,22 @@
+if 5 > 2:
+    print("Five is greater than two")
+if 5 > 2:
+        print("Five is greater than two!")
+
+x = 5
+y = "Hello World!"
+
+#This is a comment.
+#print("Hello, World!")
+# This a comment
+# Written in
+# More than just one line
+
+"""
+This is a comment.
+print("Hello, World!")
+This a comment
+Written in
+More than just one line
+"""
+print("Hello world!") # This is another comment 
---
+++ PythonVariable.py
@@ -0,0 +1,103 @@
+x = 5 
+y = "John" 
+print(x)
+print(y)
+
+x = 4 # x is of type int
+y = "Sally" #x is now of tyoe str
+print(x)
+print(y)
+
+x = str(3) #x will be '3'
+y = int(3) #x will be 3 
+z = float(3) #x will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+#is the same
+x = 'John'
+
+a = 4
+A = "Sally"
+# A will not overwrite a
+print(a)
+print(A)
+
+####### Variable Names ########
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# CAMEL CASE # 
+myVariableName = "John"
+#Pascal Case#
+MyVariableName = "John"
+#Snake Casa#
+my_variable_name = "John"
+
+# Many Values to Multiple Variables #
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+#Python - Output Variables#
+
+x = "Python is awesome"
+print(x)
+
+x = "Python" 
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python" 
+y = "is"
+z = "awesome"
+print(x+y+z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
